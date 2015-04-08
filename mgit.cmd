c46adefd2c6c4bf84e88bac5505a1604e397df09
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-04-08 03:45:12 +0300

    unimportant

--- mgit.cmd
+++ mgit.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
-rem find sh.exe from a git installation and run our git wrapper with it.
-rem for this to work git.exe must be in PATH and sh.exe must be in ../bin.
+rem find bash.exe from a git installation and run our git wrapper with it.
+rem for this to work git.exe must be in PATH and bash.exe must be in ../bin.
 :begin
 	call :set_dir git.exe
 	if exist !dir! goto git_found
@@ -11,7 +11,7 @@ rem for this to work git.exe must be in PATH and sh.exe must be in ../bin.
 	goto end
 :git_found
 	set PATH=/bin
-	"%dir:~0,-12%\bin\sh.exe" "%~dp0mg" %*
+	"%dir:~0,-12%\bin\bash.exe" "%~dp0mgit" %*
 	goto end
 :git_not_found
 	echo git.exe not found in PATH
